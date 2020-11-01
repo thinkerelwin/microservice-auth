c13186b3c82f41455b405a79db131989435b3192
Huang Chi-Heng <huangchi-heng@Huangs-MacBook-Pro.local>
2020-11-01 08:54:08 +0800

    auto updates

--- src/events/subjects.ts
+++ src/events/subjects.ts
@@ -1,7 +1,11 @@
 export enum Subjects {
   TicketCreated = "ticket:created",
   TicketUpdated = "ticket:updated",
+
   OrderCreated = "order:created",
   OrderCancelled = "order:cancelled",
-  ExpirationComplete = 'expiration:complete'
+
+  ExpirationComplete = 'expiration:complete',
+
+  PaymentCreated = 'payment:created'
 }
